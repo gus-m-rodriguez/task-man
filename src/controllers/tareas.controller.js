3903f57320313971f5394d2f505ff9cb61b2df00
Gustavo <150718843+gus-m-rodriguez@users.noreply.github.com>
2025-10-27 20:25:29 -0300

    Actualizar tarea, crear tarea, borrar tarea y listar tareas todo ok, clase 2 video 9

--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -1,5 +1,66 @@
-export const listarTareas = (req, res) => res.send("obteniendo tareas");
-export const listarTarea = (req, res) => res.send("obteniendo tarea unica");
-export const crearTarea = (req, res) => res.send("creando tarea");
-export const actualizarTarea = (req, res) => res.send("actualizando tarea unica");
-export const eliminarTarea = (req, res) => res.send("eliminando tarea unica");
+import { pool } from "../db.js";
+
+export const listarTareas = async (req, res) => {
+   
+        const resultado = await pool.query("SELECT * FROM tareas");
+        console.log(resultado);
+        return res.json(resultado.rows);
+}
+
+export const listarTarea = async (req, res) => {
+    const resultado = await pool.query("SELECT * FROM tareas WHERE id = $1", [req.params.id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: "tarea no encontrada"
+        });
+    }
+    return res.json(resultado.rows[0]);
+}
+
+export const crearTarea = async (req, res, next) => {
+    const { titulo, descripcion } = req.body;
+    
+    
+    try {    
+        const result = await pool.query("INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *", [titulo, descripcion]);
+        res.json(result.rows[0]);
+        console.log(result.rows[0]);
+    } catch (error) {
+        if (error.code === "23505") {
+            return res.status(409).json({
+                message: "ya existe una tarea con ese titulo"
+            });
+        }
+
+        console.log(error);
+        next(error);
+    } 
+    
+}
+
+export const actualizarTarea = async (req, res) => {
+    const { titulo, descripcion } = req.body;
+    const id = req.params.id;
+    const resultado = await pool.query("UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3 RETURNING *", [titulo, descripcion, id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: "tarea no encontrada con ese id"
+        });
+    }
+    return res.json(resultado.rows[0]);
+}
+
+
+
+
+
+export const eliminarTarea = async (req, res) => {
+    const resultado = await pool.query("DELETE FROM tareas WHERE id = $1", [req.params.id]);
+    
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: "tarea no encontrada"
+        });
+    }
+    return res.sendStatus(204);
+}
--- src/router/tareas.routes.js
+++ src/router/tareas.routes.js
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import { listarTareas, listarTarea, crearTarea, actualizarTarea, eliminarTarea } from "../controllers/tareas.controller";
+import Router from "express-promise-router";
+import { listarTareas, listarTarea, crearTarea, actualizarTarea, eliminarTarea } from "../controllers/tareas.controller.js";
 
 
 const router = Router();

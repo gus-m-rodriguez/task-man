d24450366099b512889a89cc5d843fc88ae911b2
Gustavo <150718843+gus-m-rodriguez@users.noreply.github.com>
2025-10-28 19:21:15 -0300

    signin out up y profile ok, clase 9 video 3 ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,9 +2,36 @@ import { pool } from "../db.js";
 import bcrypt from "bcrypt";
 import {createAccessToken} from "../libs/jwt.js";
 
-export const signin = (req, res) => res.send("ingresando");
+// SIGN IN
+export const signin = async (req, res) => {
+   const {email, password} = req.body;
 
-export const signup = async (req, res) => {
+   const result = await pool.query("SELECT * FROM usuarios WHERE email =$1", [email]);
+
+   if(result.rowCount === 0){
+        return res.status(400).json({message:"El correo no esta registrado"});
+   }
+
+   const validPassword = await bcrypt.compare(password, result.rows[0].password);
+
+   if(!validPassword){
+        return res.status(400).json({message: "La constraseña es incorrecta"});
+    }
+
+    const token = await createAccessToken({id: result.rows[0].id});
+    console.log(result);
+    res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "none",
+        //1 dia milisegundos, segundos, minutos, horas
+        maxAge: 1000 * 60 * 60 * 24 //1 dia
+        });
+        return res.json(result.rows[0]);     
+}
+
+//SIGN UP
+
+export const signup = async (req, res, next) => {
     const {nombre, email, password} = req.body;
     
     try{
@@ -29,12 +56,23 @@ export const signup = async (req, res) => {
                 message: "El correo ya esta registrado"
             });
         }
+        next(error);
         
     }
         
     
 };
 
-export const signout = (req, res) => res.send("cerrando sesion");
+// SIGNOUT
 
-export const profile = (req, res) => res.send("perfil del usuario");
+export const signout = (req, res) => {
+    res.clearCookie("token");
+    return res.json({message: "Sesion cerrada"});
+};
+
+//PROFILE
+export const profile = async (req, res) => {
+    const resultado = await pool.query("SELECT * FROM usuarios WHERE id =$1", [req.usuarioId]);
+    return res.json(resultado.rows[0]);
+
+};
